1f1470d55d48a652c590a6465a92f88530f9f7ff
NikolaiFufaev <98405572+NikolaiFufaev@users.noreply.github.com>
2024-07-22 16:42:55 +0300

    update

--- src/main/java/com/example/aston_app_homework/AppController.java
+++ src/main/java/com/example/aston_app_homework/AppController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AppController {
 
     @GetMapping
-    public String greet() {
+    public String hello() {
         return "Hello world ";
     }
 
