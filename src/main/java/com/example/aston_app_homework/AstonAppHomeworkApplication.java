408cd72675da8ee4221f56e25260153ddd67af7c
NikolaiFufaev <98405572+NikolaiFufaev@users.noreply.github.com>
2024-07-22 14:06:28 +0300

    update

--- src/main/java/com/example/aston_app_homework/AstonAppHomeworkApplication.java
+++ src/main/java/com/example/aston_app_homework/AstonAppHomeworkApplication.java
@@ -1,18 +1,13 @@
 package com.example.aston_app_homework;
 
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication(exclude = JacksonAutoConfiguration.class)
-public class AstonAppHomeworkApplication extends SpringBootServletInitializer {
-  	public static void main(String[] args) {
-		SpringApplicationBuilder builder = new SpringApplicationBuilder(AstonAppHomeworkApplication.class).web(WebApplicationType.SERVLET);
-		ConfigurableApplicationContext context = builder.build().run(args);
-	}
+@SpringBootApplication()
+public class AstonAppHomeworkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AstonAppHomeworkApplication.class, args);
+    }
 
 
 }
