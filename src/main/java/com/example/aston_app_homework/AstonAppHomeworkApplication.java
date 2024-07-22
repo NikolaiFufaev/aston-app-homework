f70779cd4ae8cbaeab8aff68ea7bde5aee2d89bc
NikolaiFufaev <98405572+NikolaiFufaev@users.noreply.github.com>
2024-07-22 12:51:50 +0300

    update

--- src/main/java/com/example/aston_app_homework/AstonAppHomeworkApplication.java
+++ src/main/java/com/example/aston_app_homework/AstonAppHomeworkApplication.java
@@ -2,9 +2,15 @@ package com.example.aston_app_homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AstonAppHomeworkApplication {
+public class AstonAppHomeworkApplication extends SpringBootServletInitializer {
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(AstonAppHomeworkApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AstonAppHomeworkApplication.class, args);
