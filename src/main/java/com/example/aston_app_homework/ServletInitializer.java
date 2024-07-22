f0c322afea64b3b0268fdb07293ca13f52f23257
NikolaiFufaev <98405572+NikolaiFufaev@users.noreply.github.com>
2024-07-22 11:59:20 +0300

    update

---
+++ src/main/java/com/example/aston_app_homework/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.aston_app_homework;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AstonAppHomeworkApplication.class);
+	}
+
+}
---
+++ src/test/java/com/example/aston_app_homework/AstonAppHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aston_app_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonAppHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
